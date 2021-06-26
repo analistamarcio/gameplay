1dfcaa5bec78e2f179a0280035179a59dcc43d47
Marcio Ferreira <analistamarcio@hotmail.com>
2021-06-26 10:23:00 -0700

    Refactoring after lesson 4.

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -7,13 +7,11 @@ import React, {
 
 import * as AuthSession from "expo-auth-session";
 
-import {
-	SCOPE,
-	CLIENT_ID,
-	CDN_IMAGE,
-	REDIRECT_URI,
-	RESPONSE_TYPE,
-} from "../configs";
+const { SCOPE } = process.env;
+const { CLIENT_ID } = process.env;
+const { CDN_IMAGE } = process.env;
+const { REDIRECT_URI } = process.env;
+const { RESPONSE_TYPE } = process.env;
 
 import { api } from "../services/api";
 
@@ -38,7 +36,8 @@ type AuthProviderProps = {
 
 type AuthorizationResponse = AuthSession.AuthSessionResult & {
 	params: {
-		access_token: string;
+		access_token?: string;
+		error?: "string;"
 	}
 }
 
@@ -56,7 +55,7 @@ function AuthProvider({ children }: AuthProviderProps) {
 
 			const { type, params } = await AuthSession.startAsync({ authUrl }) as AuthorizationResponse;
 
-			if (type === "success") {
+			if (type === "success" && !params.error) {
 				api.defaults.headers.authorization = `Bearer ${params.access_token}`;
 
 				const userInfo = await api.get("/users/@me");
@@ -70,10 +69,10 @@ function AuthProvider({ children }: AuthProviderProps) {
 					token: params.access_token,
 				});
 			}
-			setLoading(false);
-
 		} catch {
 			throw new Error("Authentication failed.");
+		} finally {
+			setLoading(false);
 		}
 	}
 	
